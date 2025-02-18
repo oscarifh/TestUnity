76dd38c21ee2a3e80b27c87246ad84fa9f8a0009
oscarifh <oscarifuenteshernandez@gmail.com>
2025-02-18 17:14:29 -0600

    update git ignore file
    Added path directory to some folders

---
+++ Myproject/Assets/Scenes/Scripts/HolaMundo.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+        Debug.LogWarning("Ejemplo de Warning");
+        Debug.LogError("Ejemplo de error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
